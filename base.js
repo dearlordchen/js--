8a2cac1477c63957cb2f16db6f9be0364d61bbf8
lordchen <timchenlei@qq.com>
2013-11-09 15:24:18 +0800

    添加一个基础库文件base.js

---
+++ base.js
@@ -0,0 +1,11 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: lordchen
+ * Date: 13-11-9
+ * Time: 下午3:14
+ * To change this template use File | Settings | File Templates.
+ */
+
+(function(w){
+    alert(w.location);
+})(window)
